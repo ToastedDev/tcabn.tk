b3f4901fb3cc4c36603388e021a4a58c979b2af6
Not TCA <realtcatech@gmail.com>
2022-10-08 18:07:49 +0800

    fix text centering for mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen">
+    <div className="flex flex-col justify-center items-center text-center h-screen">
       <h1 className="text-6xl font-bold">Coming soon...</h1>
       <h2 className="text-lg mt-1">
         Keep your eyes pealed on this website and the{" "}
